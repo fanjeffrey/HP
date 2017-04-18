8f44ec228482257b86b46ad7c835b3e5780784ae
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-04-18 23:32:49 +0800

    initial commit

---
+++ union-online/src/HPCN.UnionOnline/HPCN.UnionOnline/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HPCN.UnionOnline
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .UseApplicationInsights()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
