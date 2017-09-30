2def0d99f7509dfcb3801c737aa59fb0c62f88e1
Jeffrey Fan <jiangzhu.fan@outlook.com>
2017-09-30 11:24:59 +0800

    move QueryEmployee to Enrolling

--- union-online/src/HPCN.UnionOnline/HPCN.UnionOnline/Site/Controllers/EnrollingController.cs
+++ union-online/src/HPCN.UnionOnline/HPCN.UnionOnline/Site/Controllers/EnrollingController.cs
@@ -16,17 +16,20 @@ namespace HPCN.UnionOnline.Site.Controllers
         private readonly IEnrollingService _enrollingService;
         private readonly IEnrollmentService _enrollmentService;
         private readonly IUserService _userSerivce;
+        private readonly IEmployeeService _employeeService;
         private readonly ILogger _logger;
 
         public EnrollingController(
             IEnrollingService enrollingService,
             IEnrollmentService enrollmentService,
             IUserService userService,
+            IEmployeeService employeeService,
             ILoggerFactory loggerFactory)
         {
             _enrollingService = enrollingService;
             _enrollmentService = enrollmentService;
             _userSerivce = userService;
+            _employeeService = employeeService;
             _logger = loggerFactory.CreateLogger<EnrollingController>();
         }
 
@@ -152,6 +155,37 @@ namespace HPCN.UnionOnline.Site.Controllers
             };
 
             return View(model);
+        }
+
+        public async Task<IActionResult> QueryEmployee(string no)
+        {
+            if (string.IsNullOrWhiteSpace(no))
+            {
+                return Json(new
+                {
+                    emailAddress = string.Empty,
+                    name = string.Empty,
+                    phoneNumber = string.Empty
+                });
+            }
+
+            var employee = await _employeeService.GetAsync(no);
+            if (employee == null)
+            {
+                return Json(new
+                {
+                    emailAddress = string.Empty,
+                    name = string.Empty,
+                    phoneNumber = string.Empty
+                });
+            }
+
+            return Json(new
+            {
+                emailAddress = employee.EmailAddress,
+                name = employee.ChineseName,
+                phoneNumber = employee.PhoneNumber
+            });
         }
     }
 }
